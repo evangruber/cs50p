2c24be93f426ba8992194db79b22f28a2227cca3
evangruber <emgruber.dev@gmail.com>
2026-06-27 23:10:14 +0800

    Add test_fuel.py solution and fuel.py file to be tested

--- week 5/test_fuel/fuel.py
+++ week 5/test_fuel/fuel.py
@@ -1,13 +1,41 @@
 def main():
-    ...
+
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            percentage = convert(fraction)
+            break
+        except (ValueError, ZeroDivisionError):
+            continue
+
+    print(gauge(percentage))
 
 
 def convert(fraction):
-    ...
+    x, y = fraction.split("/")
+
+    x = int(x)
+    y = int(y)
+
+    if y == 0:
+        raise ZeroDivisionError
+
+    if x > y or x < 0:
+        raise ValueError
+
+    percentage = int(round((x / y) * 100))
+
+    return percentage
 
 
 def gauge(percentage):
-    ...
+
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
 
 
 if __name__ == "__main__":
--- week 5/test_fuel/test_fuel.py
+++ week 5/test_fuel/test_fuel.py
@@ -1,4 +1,52 @@
 from fuel import convert, gauge
+import pytest
 
 
-...
+def test_convert_invalid_format():
+    assert convert("1/4") == 25
+
+    with pytest.raises(ValueError):
+        convert("1//4")
+
+
+def test_convert_valid_fractions():
+    assert convert("2/4") == 50
+    assert convert("3/4") == 75
+    assert convert("4/4") == 100
+
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+    with pytest.raises(ValueError):
+        convert("5/4")
+
+
+def test_convert_negative_fractions():
+    with pytest.raises(ValueError):
+        convert("-1/4")
+
+    with pytest.raises(ValueError):
+        convert("1/-4")
+
+    with pytest.raises(ValueError):
+        convert("-1/-4")
+
+
+def test_convert_words():
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+
+    with pytest.raises(ValueError):
+        convert("1/dog")
+
+    with pytest.raises(ValueError):
+        convert("cat/4")
+
+
+def test_gauge():
+    assert gauge(0) == "E"
+    assert gauge(1) == "E"
+    assert gauge(2) == "2%"
+    assert gauge(98) == "98%"
+    assert gauge(99) == "F"
+    assert gauge(100) == "F"
