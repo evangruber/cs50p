d99c484211a6bbffee7d5de2eddde6e6d8be1ad5
evangruber <emgruber.dev@gmail.com>
2026-06-27 13:57:32 +0800

    Add test_plates.py solution and plates.py file to be tested

--- week 5/test_plates/plates.py
+++ week 5/test_plates/plates.py
@@ -1,9 +1,33 @@
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    if not (2 <= len(s) <= 6):
+        return False
+
+    if not s[:2].isalpha() or not s.isalnum():
+        return False
+
+    digit_started = False
+
+    for c in s[2:]:
+
+        if c.isdigit():
+            if not digit_started:
+                digit_started = True
+
+                if c == "0":
+                    return False
+
+        elif digit_started:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
--- week 5/test_plates/test_plates.py
+++ week 5/test_plates/test_plates.py
@@ -1,4 +1,42 @@
 from plates import is_valid
 
 
-...
+def test_is_valid_plate_length():
+    assert not is_valid("")
+    assert not is_valid("C")
+    assert is_valid("CS")
+    assert is_valid("CSPYTH")
+    assert not is_valid("CSPYTHO")
+
+
+def test_is_valid_first_two_isalpha():
+    assert is_valid("CS")
+    assert is_valid("CSP")
+    assert is_valid("CS5")
+    assert not is_valid("5CSP")
+    assert not is_valid("C5PY")
+    assert not is_valid("50")
+
+
+def test_is_valid_no_punctuation():
+    assert not is_valid("CS5!)")
+    assert not is_valid("CSP.2/")
+    assert not is_valid("CS,PY")
+
+
+def test_is_valid_no_whitespace():
+    assert not is_valid("  CSP")
+    assert not is_valid("CSP  ")
+    assert not is_valid(" CSP ")
+
+
+def test_is_valid_digit_started_nonzero():
+    assert is_valid("CS50")
+    assert is_valid("CS500")
+    assert not is_valid("CS05")
+
+
+def test_is_valid_digit_started_noalpha():
+    assert is_valid("CS5012")
+    assert not is_valid("CS50P")
+    assert not is_valid("CS50P1")
