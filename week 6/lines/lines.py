48f2648ee7a4194aea84b440fd9ac74b22a5f7cf
evangruber <emgruber.dev@gmail.com>
2026-06-29 00:05:24 +0800

    Add lines.py solution

--- week 6/lines/lines.py
+++ week 6/lines/lines.py
@@ -1,5 +1,34 @@
+import sys
+
+
 def main():
-    ...
+
+    filename = validate_input()
+
+    try:
+        with open(filename, "r") as file:
+            code_lines = [
+                line
+                for line in file
+                if not line.lstrip().startswith("#") and line.strip()
+            ]
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+    print(len(code_lines))
+
+
+def validate_input():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")
+
+    return sys.argv[1]
 
 
 if __name__ == "__main__":
