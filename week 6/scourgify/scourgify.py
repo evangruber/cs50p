147c176344a8cb1b888d6b7e2010ab13cafbcf85
evangruber <emgruber.dev@gmail.com>
2026-06-30 23:52:03 +0800

    ADD scourgify.py (Incomplete) NEED to fix READ -> process -> WRITE

--- week 6/scourgify/scourgify.py
+++ week 6/scourgify/scourgify.py
@@ -1,5 +1,45 @@
+import sys
+import csv
+
+
 def main():
-    ...
+    filename = validate_input()
+    student_record = {}
+    header = False
+
+    try:
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                last_name, first_name = (
+                    row[reader.fieldnames[0]].replace('\"', "").split(",")
+                )
+                with open(sys.argv[2], "w", newline="") as new_csvfile:
+                    writer = csv.DictWriter(
+                        new_csvfile, fieldnames=["first", "last", reader.fieldnames[1]]
+                    )
+                    if not header:
+                        writer.writeheader()
+                        header = True
+                    writer.writerow(
+                        {
+                            "first": first_name.strip(),
+                            "last": last_name.strip(),
+                            f"{reader.fieldnames[1]}": row[reader.fieldnames[1]].strip(),
+                        }
+                    )
+    except FileNotFoundError:
+        sys.exit(f"Could not read {filename}")
+
+
+def validate_input():
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+
+    return sys.argv[1]
 
 
 if __name__ == "__main__":
