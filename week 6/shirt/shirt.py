413bffc90355413735c86a64b4a6d5c4a1cf0806
evangruber <emgruber.dev@gmail.com>
2026-07-01 22:44:25 +0800

    ADD shirt.py input validation function

--- week 6/shirt/shirt.py
+++ week 6/shirt/shirt.py
@@ -1,5 +1,31 @@
+import sys
+
+
 def main():
-    ...
+    filename = validate_input()
+    
+    try:
+        ...
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+
+
+def validate_input():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+
+    if not sys.argv[1].endswith((".jpg", ".jpeg", ".png")) or not sys.argv[2].endswith(
+        (".jpg", ".jpeg", ".png")
+    ):
+        sys.exit("Invalid output")
+
+    if sys.argv[1].split(".")[-1].lower() != sys.argv[2].split(".")[-1].lower():
+        sys.exit("Input and output have different extensions")
+
+    return sys.argv[1]
 
 
 if __name__ == "__main__":
